89747c05b51d36204fdea68cab1708a6e76e4c9f
FastiGun <benjamin760080@gmail.com>
2023-07-18 10:36:16 +0200

    ajout du eslint et des controller pour les user et reservations

---
+++ src/controllers/reservationController.js
@@ -0,0 +1,131 @@
+const Reservation = require('../models/reservationModel');
+
+// Fonction pour créer une nouvelle réservation
+const createReservation = async (req, res) => {
+  try {
+    const { nomLocataire, prenomLocataire, dateArrivee, dateDepart, nombrePersonne, dureeLocation } = req.body;
+
+    const existingReservation = await Reservation.findOne({
+        $or: [
+          {
+            dateArrivee: { $gte: dateArrivee, $lte: dateDepart },
+          },
+          {
+            dateDepart: { $gte: dateArrivee, $lte: dateDepart },
+          },
+        ],
+      });
+  
+      if (existingReservation) {
+        return res.status(409).json({ message: 'Il y a déjà une réservation pour ces dates' });
+      }
+
+    const reservation = new Reservation({
+      nomLocataire,
+      prenomLocataire,
+      dateArrivee,
+      dateDepart,
+      nombrePersonne,
+      dureeLocation
+    });
+
+    await reservation.save();
+    res.status(201).json({ message: 'Réservation créée avec succès : ', reservation });
+  } catch (error) {
+    res.status(500).json({ message: 'Erreur lors de la création de la réservation : ', error });
+  }
+};
+
+// Fonction pour récupérer les informations d'une réservation
+const getReservation = async (req, res) => {
+  try {
+    const reservationId = req.params.id;
+    const reservation = await Reservation.findById(reservationId);
+    if (!reservation) {
+      return res.status(404).json({ message: 'Réservation introuvable' });
+    }
+    res.json({ reservation });
+  } catch (error) {
+    res.status(500).json({ message: 'Erreur lors de la récupération de la réservation : ', error });
+  }
+};
+
+// Fonction pour récupérer une réservation par ses dates d'arrivée et de départ
+const getReservationByDates = async (req, res) => {
+    try {
+      const { dateArrivee, dateDepart } = req.body;
+      const reservation = await Reservation.findOne({
+        dateArrivee: { $lte: dateDepart },
+        dateDepart: { $gte: dateArrivee },
+      });
+      if (!reservation) {
+        return res.status(404).json({ message: 'Réservation introuvable pour ces dates' });
+      }
+      res.json({ reservation });
+    } catch (error) {
+      res.status(500).json({ message: 'Erreur lors de la récupération de la réservation', error });
+    }
+  };
+
+  const getReservationByDay = async (req, res) => {
+    try {
+      const { date } = req.params;
+      const reservation = await Reservation.findOne({
+        dateArrivee: { $lte: date },
+        dateDepart: { $gte: date },
+      });
+      if (!reservation) {
+        return res.status(404).json({ message: 'Aucune réservation pour cette journée' });
+      }
+      res.json({ reservation });
+    } catch (error) {
+      res.status(500).json({ message: 'Erreur lors de la récupération de la réservation', error });
+    }
+  };
+
+// Fonction pour modifier les informations d'une réservation
+const updateReservation = async (req, res) => {
+  try {
+    const reservationId = req.params.id;
+    const { nomLocataire, prenomLocataire, dateArrivee, dateDepart, nombrePersonne, dureeLocation } = req.body;
+
+    const reservation = await Reservation.findByIdAndUpdate(reservationId, {
+      nomLocataire,
+      prenomLocataire,
+      dateArrivee,
+      dateDepart,
+      nombrePersonne,
+      dureeLocation
+    });
+
+    if (!reservation) {
+      return res.status(404).json({ message: 'Réservation introuvable' });
+    }
+
+    res.status(200).json({ message: 'Réservation modifiée avec succès :', reservation });
+  } catch (error) {
+    res.status(500).json({ message: 'Erreur lors de la modification de la réservation : ', error });
+  }
+};
+
+const deleteReservation = async (req, res) => {
+  try {
+    const reservationId = req.params.id;
+    const reservation = await Reservation.findByIdAndDelete(reservationId);
+    if (!reservation) {
+      return res.status(404).json({ message: 'Réservation introuvable' });
+    }
+    res.status(200).json({ message: 'Réservation supprimée avec succès' });
+  } catch (error) {
+    res.status(500).json({ message: 'Erreur lors de la suppression de la réservation : ', error });
+  }
+}
+
+module.exports = {
+  createReservation,
+  getReservation,
+  getReservationByDates,
+  getReservationByDay,
+  updateReservation,
+  deleteReservation
+};
---
+++ src/controllers/userController.js
@@ -0,0 +1,60 @@
+const Utilisateur = require('../models/userModel');
+
+// Fonction pour créer un nouvel utilisateur
+const createUtilisateur = async (req, res) => {
+    try {
+        const { email, motDePasse, prenom, nom } = req.body;
+
+        const utilisateur = new Utilisateur({
+            email,
+            motDePasse,
+            prenom,
+            nom
+        });
+        await utilisateur.save();
+        res.status(201).json({ message: 'Utilisateur créé avec succès', utilisateur });
+    } catch (error) {
+        res.status(500).json({ message: 'Erreur lors de la création de l\'utilisateur', error });
+    }
+};
+
+// Fonction pour récupérer les informations d'un utilisateur
+const getUtilisateur = async (req, res) => {
+    try {
+        const userId = req.params.id;
+        const utilisateur = await Utilisateur.findById(userId);
+        if (!utilisateur) {
+            return res.status(404).json({ message: 'Utilisateur introuvable' });
+        }
+        res.json({ utilisateur });
+    } catch (error) {
+        res.status(500).json({ message: 'Erreur lors de la récupération de l\'utilisateur', error });
+    }
+};
+
+// Fonction pour modifier les informations d'un utilisateur
+const updateUtilisateur = async (req, res) => {
+    try {
+        const userId = req.params.id;
+        const { motDePasse, prenom, nom } = req.body;
+        const utilisateur = await Utilisateur.findByIdAndUpdate(userId, {
+            motDePasse,
+            prenom,
+            nom
+        });
+        if (!utilisateur) {
+            return res.status(404).json({ message: 'Utilisateur introuvable' });
+        }
+        await utilisateur.save();
+        res.status(200).json({ message: 'Utilisateur modifié avec succès' });
+    } catch (error) {
+        res.status(500).json({ message: 'Erreur lors de la modification de l\'utilisateur', error });
+    }
+}
+
+
+module.exports = {
+    createUtilisateur,
+    getUtilisateur,
+    updateUtilisateur,
+};
